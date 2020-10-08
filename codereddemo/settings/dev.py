150645882f950b6afc70c57f7a46491c290ec12c
Dustin Bleile <dustinbleile@gmail.com>
2020-10-08 15:07:34 +0000

    initial coderedcms

---
+++ codereddemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8&5)d^-_*#4xxzzc_sc3=t=f&yu#k5tby$d)81_f(6sc#4lysc'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
